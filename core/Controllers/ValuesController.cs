11521442b1ebbd7d1ff370bfb404a52ff3b94951
Felipe Cardozo <felipe.cardozo@sapiensia.com>
2020-02-21 17:51:13 -0300

    .

---
+++ core/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibrary1;
+using Microsoft.AspNetCore.Mvc;
+using standard;
+
+namespace core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var coiso = new StandardPoc();
+
+            coiso.FrameworkPoc = new FrameworkPoc();
+            coiso.FrameworkPoc.Coiso = "TESTE";
+
+
+            return coiso.FrameworkPoc.Coiso;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ standard/StandardPoc.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace standard
+{
+    public class StandardPoc
+    {
+        public FrameworkPoc FrameworkPoc { get; set; }
+    }
+}
